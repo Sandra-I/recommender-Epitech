f51510a077243aeaa4abb1b4f4c77b3b8cef4b53
Sandra-I <sandra.ibrahim@hotmail.fr>
2022-01-29 18:26:02 +0100

    Feat: adding new csv file

--- recommender-service/customer_segmenter.py
+++ recommender-service/customer_segmenter.py
@@ -20,11 +20,12 @@ class CustomerSegmenter:
         # DEV to delete or comment for prod
         # self.df = pd.read_csv('../clean_dataset.csv', parse_dates=True)
         # self.df_customer_details = pd.read_csv('../csv_details_by_customer.csv')
-        self.df_global_metrics = pd.read_csv('../csv_global_metrics.csv')
+        # self.df_global_metrics = pd.read_csv('../csv_global_metrics.csv')
 
         # PROD to uncomment for production
         self.df = self.get_file_from_google.get_clean_dataframe()
         self.df_customer_details = self.get_file_from_google.get_csv_details_by_customer()
+        self.df_global_metrics = self.get_file_from_google.get_csv_global_metrics()
 
         self.customers_id = self.get_customers_id()
         print(self.df.head())
